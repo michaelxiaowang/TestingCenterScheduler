8a4bc4f3b920c508c148a32c3eb16fa518d61879
michaelxiaowang <michaelxiaowang@gmail.com>
2015-11-01 17:20:39 -0500

    added appointment manager

---
+++ src/appointment-manager.js
@@ -0,0 +1,22 @@
+var db = require('./db');
+var exams = db.collection('exams');
+var appointments = db.collection('appointments');
+
+//Exported functions can be used by other file that require this module
+
+/*Creates a new appointment*/
+exports.studentCreateAppointment = function(req) {
+	exams.findOne({examID: req.body.exam}, function(err, exam) {
+		if(err) {
+			console.log(err);
+		}
+		console.log(exam);
+		var examID 	= req.body.exam;
+		var date = new Date(req.body.month + " " + req.body.day + (new Date).getFullYear().toString());
+		if(req.body.ampm == 'pm') {
+			req.body.hour += 12;
+		}
+		//var duration = req.body.hour*3600000 + req.body.minute*60000 + exam.duration;
+
+	});
+}
